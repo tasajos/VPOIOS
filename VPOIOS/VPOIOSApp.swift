83c2e09faa203d2597bd00008651383e6aaac903
Carlos Azcarraga <tasajos@gmail.com>
2024-05-28 09:39:59 -0700

    Initial Commit

---
+++ VPOIOS/VPOIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  VPOIOSApp.swift
+//  VPOIOS
+//
+//  Created by admin on 5/28/24.
+//
+
+import SwiftUI
+
+@main
+struct VPOIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
